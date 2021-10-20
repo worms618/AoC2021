b8fffbf1485c46f9e41e7c4c779d044686102b2d
Remco Vorthoren <remco.boys@gmail.com>
2021-10-20 19:36:42 +0200

    Create a script to create a directory with files from a template directory and files for a day

---
+++ scripts/createDay.py
@@ -0,0 +1,48 @@
+# Decide which day
+# Check if day already exists
+# If exists, stop
+# If not exists:
+# copy _days# and content towards day# where # will be changed with day number
+import os
+import sys
+
+daysRootDir = os.path.join('..', 'src')
+replaceWithDayNumberChar = '$'
+
+# Days template directory with content
+daysTemplateDirName = '_day' + replaceWithDayNumberChar
+daysTemplateDirPath = os.path.join(daysRootDir, daysTemplateDirName)
+
+day = 1
+
+dayDirName = daysTemplateDirName.replace('_', '').replace(replaceWithDayNumberChar, str(day))
+dayDirPath = os.path.join(daysRootDir, dayDirName)
+
+# Create the day directory, when not exists
+if not os.path.isdir(dayDirPath):
+    os.mkdir(dayDirPath)
+
+dayTemplateFileNames = os.listdir(daysTemplateDirPath)
+
+# Copy the template files and
+# Replace replaceWithDayNumberChar with the day number in either file name and file content
+for dayTemplateFileName in dayTemplateFileNames:
+    # New file name and path for day directory
+    dayFileName = dayTemplateFileName.replace(replaceWithDayNumberChar, str(day))
+    dayFilePath = os.path.join(dayDirPath, dayFileName)
+
+    # Create new file for day, if not exists
+    if not os.path.isfile(dayFilePath):
+        dayTemplateFilePath = os.path.join(daysTemplateDirPath, dayTemplateFileName)
+
+        # Read template file content
+        dayTemplateFileContent = ''
+        with open(dayTemplateFilePath) as f:
+            dayTemplateFileContent = f.read()
+
+        # Set the day number where needed in the file content
+        dayTemplateFileContent = dayTemplateFileContent.replace(replaceWithDayNumberChar, str(day))
+
+        # Create new file for day
+        with open(dayFilePath, mode="w") as f:
+            f.write(dayTemplateFileContent)
--- src/_day$/day$.py
+++ src/_day$/day$.py
@@ -4,8 +4,8 @@ with open('./input.txt') as f:
 
 # Part 1
 resultPart1 = 0
-print('Anwser day # part 1:', resultPart1)
+print('Anwser day $ part 1:', resultPart1)
 
 # Part 2
 resultPart2 = 0
-print('Anwser day # part 2:', resultPart2)
+print('Anwser day $ part 2:', resultPart2)
