6fb49cae691582512df31718c12da32ddc02a10e
Remco Vorthoren <remco.boys@gmail.com>
2021-10-20 18:57:08 +0200

    Place template files for the scripts

---
+++ src/_day#/day#.py
@@ -0,0 +1,11 @@
+lines = []
+with open('./input.txt') as f:
+    lines = [line.strip() for line in f]
+
+# Part 1
+resultPart1 = 0
+print('Anwser day # part 1:', resultPart1)
+
+# Part 2
+resultPart2 = 0
+print('Anwser day # part 2:', resultPart2)
