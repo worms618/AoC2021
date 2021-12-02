ed8585fc7be391e417fed95feafb37bb8d8c9eab
Remco Vorthoren <remco.boys@gmail.com>
2021-12-02 08:29:24 +0100

    Add solution for part 1

--- src/day2/day2.py
+++ src/day2/day2.py
@@ -14,10 +14,45 @@ def getInputLines():
 # --- </Do not edit> ---
 
 
+def calcNextPos(command, position):
+    cmdIdentifier = command[0]
+    cmdUnits = command[1]
+
+    posHorizontal = position[0]
+    posDepth = position[1]
+
+    if cmdIdentifier.startswith('f'):
+        posHorizontal = posHorizontal + cmdUnits
+    elif cmdIdentifier.startswith('d'):
+        posDepth = posDepth + cmdUnits
+    elif cmdIdentifier.startswith('u'):
+        posDepth = posDepth - cmdUnits
+
+    return (posHorizontal, posDepth)
+
+
+def createCommand(line):
+    commandParts = line.split(" ", 1)
+    # Index 0: identifier
+    # Index 1: units
+    return (commandParts[0], int(commandParts[1]))
+
+
 lines = getInputLines()
 
+# Index 0: horizontal
+# Index 1: depth
+position = (0, 0)
+
 # Part 1
-resultPart1 = 0
+position = (0, 0)
+
+for line in lines:
+    command = createCommand(line)
+    position = calcNextPos(command, position)
+
+# horizontal * depth
+resultPart1 = position[0] * position[1]
 print('Anwser day 2 part 1:', resultPart1)
 
 # Part 2
