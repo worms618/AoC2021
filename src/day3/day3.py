44d9e9f650b563d8dacccd7642b962f5e8b4365e
Remco Vorthoren <remco.boys@gmail.com>
2021-12-03 08:58:42 +0100

    Add Solution for part 2

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -65,6 +65,55 @@ def getEpsilonRate(values):
 
     return epsilonRate
 
+def getOxygenGeneratorRating(values):
+    cValues = values.copy()
+
+    charIndex = 0
+    while (len(cValues) > 1) & (charIndex < len(values[0])):
+        charsAtIndex = countCharactersAtIndex(cValues, charIndex)
+        nextValue = ""
+        nextValueOccures = 0
+        for char in charsAtIndex:
+            if charsAtIndex[char] > nextValueOccures:
+                nextValue = char
+                nextValueOccures = charsAtIndex[char]
+            elif charsAtIndex[char] == nextValueOccures:
+                nextValue = "1"
+        
+        newValues = []
+        for line in cValues:
+            if line[charIndex] == nextValue:
+                newValues.append(line)
+        
+        cValues = newValues.copy()
+        charIndex = charIndex + 1
+    
+    return cValues[0]
+
+def getCO2ScrubberRating(values):
+    cValues = values.copy()
+
+    charIndex = 0
+    while (len(cValues) > 1) & (charIndex < len(values[0])):
+        charsAtIndex = countCharactersAtIndex(cValues, charIndex)
+        nextValue = ""
+        nextValueOccures = -1
+        for char in charsAtIndex:
+            if (nextValueOccures < 0) | (charsAtIndex[char] < nextValueOccures):
+                nextValue = char
+                nextValueOccures = charsAtIndex[char]
+            elif charsAtIndex[char] == nextValueOccures:
+                nextValue = "0"
+        
+        newValues = []
+        for line in cValues:
+            if line[charIndex] == nextValue:
+                newValues.append(line)
+        
+        cValues = newValues.copy()
+        charIndex = charIndex + 1
+    return cValues[0]
+
 def binaryToInt(value):
     return int(value, 2)
 
@@ -74,15 +123,24 @@ lines = getInputLines()
 
 gammaRateInBinary = getGammaRate(lines)
 epsilonRateInBinary = getEpsilonRate(lines)
-
 # print(gammaRateInBinary, epsilonRateInBinary)
 
 gammaRate = binaryToInt(gammaRateInBinary)
 epsilonRate = binaryToInt(epsilonRateInBinary)
 # print(gammaRate, epsilonRate)
+
 resultPart1 = gammaRate * epsilonRate
 print('Anwser day 3 part 1:', resultPart1)
 
 # Part 2
-resultPart2 = 0
+
+oxygenGeneratorRatingInBinary = getOxygenGeneratorRating(lines)
+CO2ScrubberRatingInBinary = getCO2ScrubberRating(lines)
+# print(oxygenGeneratorRatingInBinary, CO2ScrubberRatingInBinary)
+
+oxygenGeneratorRating = binaryToInt(oxygenGeneratorRatingInBinary)
+CO2ScrubberRating = binaryToInt(CO2ScrubberRatingInBinary)
+# print(oxygenGeneratorRating, CO2ScrubberRating)
+
+resultPart2 = oxygenGeneratorRating * CO2ScrubberRating
 print('Anwser day 3 part 2:', resultPart2)
