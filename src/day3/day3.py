b86336f3cbff45ca9e4d76bfb6752f048fc5f879
Remco Vorthoren <remco.boys@gmail.com>
2021-12-03 08:30:58 +0100

    Add solution part 1

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -13,11 +13,74 @@ def getInputLines():
     return lines
 # --- </Do not edit> ---
 
+def countCharactersAtIndex(values, index):
+    counter = {}
+
+    for chars in map(lambda x: list(x), values):
+        charAtIndex = chars[index]
+        if not charAtIndex in counter:
+            counter.setdefault(charAtIndex, 0)
+        
+        counter[charAtIndex] = counter[charAtIndex] + 1
+
+    return counter
+
+def countCharactersPerIndex(values):
+    counter = []
+    totalCharsPerValue = len(values[0])
+
+    for index in range(totalCharsPerValue):
+        counter.append(countCharactersAtIndex(values, index))
+
+    return counter
+
+def getGammaRate(values):
+    charsPerIndex = countCharactersPerIndex(values)
+    gammaRate = ""
+
+    for charsAtIndex in charsPerIndex:
+        nextValue = ""
+        nextValueOccures = 0
+        for char in charsAtIndex:
+            if charsAtIndex[char] > nextValueOccures:
+                nextValue = char
+                nextValueOccures = charsAtIndex[char]
+        
+        gammaRate = gammaRate + nextValue
+
+    return gammaRate
+
+def getEpsilonRate(values):
+    charsPerIndex = countCharactersPerIndex(values)
+    epsilonRate = ""
+
+    for charsAtIndex in charsPerIndex:
+        nextValue = ""
+        nextValueOccures = -1
+        for char in charsAtIndex:
+            if (nextValueOccures < 0) | (charsAtIndex[char] < nextValueOccures):
+                nextValue = char
+                nextValueOccures = charsAtIndex[char]
+        epsilonRate = epsilonRate + nextValue
+
+    return epsilonRate
+
+def binaryToInt(value):
+    return int(value, 2)
 
 lines = getInputLines()
 
 # Part 1
-resultPart1 = 0
+
+gammaRateInBinary = getGammaRate(lines)
+epsilonRateInBinary = getEpsilonRate(lines)
+
+# print(gammaRateInBinary, epsilonRateInBinary)
+
+gammaRate = binaryToInt(gammaRateInBinary)
+epsilonRate = binaryToInt(epsilonRateInBinary)
+# print(gammaRate, epsilonRate)
+resultPart1 = gammaRate * epsilonRate
 print('Anwser day 3 part 1:', resultPart1)
 
 # Part 2
