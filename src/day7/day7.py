8a619ba46a9ce6e049081f5e0b83d136dcf12e94
Remco Vorthoren <remco.boys@gmail.com>
2021-12-07 19:02:50 +0100

    Add solution for part 1 (brute forced?)

--- src/day7/day7.py
+++ src/day7/day7.py
@@ -13,11 +13,51 @@ def getInputLines():
     return lines
 # --- </Do not edit> ---
 
+def groupPositions(positions):
+    groupedPositions = dict()
+    # Count amount per position
+
+    for position in positions:
+        if not (position in groupedPositions):
+            groupedPositions.setdefault(position, 1)
+        else:
+            groupedPositions[position] += 1
+
+    return groupedPositions
+
+def countFuelTowardsPositions(groupedPositions, positionTowards):
+    totalFuel = 0
+
+    for position in groupedPositions:
+        multiplier = groupedPositions[position]
+        fuel = abs(positionTowards - position) * multiplier
+        totalFuel += fuel
+
+    return totalFuel
+
 
 lines = getInputLines()
+positions = list(map(int, lines[0].split(',')))
 
 # Part 1
-resultPart1 = 0
+positionsPart1 = positions.copy()
+maxPosition = max(positionsPart1)
+minPosition = min(positionsPart1)
+# print(minPosition, maxPosition)
+
+groupedPositions = groupPositions(positionsPart1)
+# print(groupedPositions)
+
+leastFuel = -1
+leastPosition = -1
+for position in groupedPositions:
+    fuel = countFuelTowardsPositions(groupedPositions, position)
+    if (leastFuel < 0) | (fuel < leastFuel):
+        leastFuel = fuel
+        leastPosition = position
+    
+# print(leastPosition)
+resultPart1 = leastFuel
 print('Anwser day 7 part 1:', resultPart1)
 
 # Part 2
