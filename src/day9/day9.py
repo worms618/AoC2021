5a6dd9925981bf87b091c1aa1ad2195507653237
Remco Vorthoren <remco.boys@gmail.com>
2021-12-09 20:17:56 +0100

    Add Solution part 2

--- src/day9/day9.py
+++ src/day9/day9.py
@@ -21,11 +21,13 @@ Y = 1
 def createVector(x=0, y=0):
     return (x, y)
 
+
 def addVectors(v1, v2):
     return (
         v1[X] + v2[X],
         v1[Y] + v2[Y]
     )
+
 
 def createHeightMap(lines):
     heightMap = []
@@ -37,57 +39,134 @@ def createHeightMap(lines):
     return heightMap
 
 
+def createProcessedMap(heightMap):
+    return list(map(lambda x: list(map(lambda _: False, x)), heightMap))
+
+
 def inboundsPoint(heightMap, point):
     if (point[Y] < 0) | (point[Y] >= len(heightMap)):
         return False
-    
+
     if (point[X] < 0) | (point[X] >= len(heightMap[point[Y]])):
         return False
 
     return True
 
 
-def getValue(heightMap, point):
-    return heightMap[point[Y]][point[X]]
+def getInboundAdjacentPoints(point, adjacentVectors):
+    adjacentPoints = []
+
+    for adjacentVector in adjacentVectors:
+        adjacentPoint = addVectors(point, adjacentVector)
+        if inboundsPoint(heightMap, adjacentPoint):
+            adjacentPoints.append(adjacentPoint)
+
+    return adjacentPoints
 
 
-def getLowPointValues(heightMap):
+def setValue(map, point, value):
+    map[point[Y]][point[X]] = value
+    return map
+
+
+def getValue(map, point):
+    return map[point[Y]][point[X]]
+
+
+def getLowPoints(heightMap):
     points = []
 
     for r in range(len(heightMap)):
         for c in range(len(heightMap[r])):
             point = createVector(c, r)
             value = getValue(heightMap, point)
-            if isLowerThanAdjacent(heightMap, directAdjacentVectors, point, value):
-                points.append(value) 
+            if isLowerThanAdjacents(heightMap, directAdjacentVectors, point, value):
+                points.append(point)
 
     return points
 
 
-def isLowerThanAdjacent(heightMap, adjacentVectors, point, value):
-    for adjacentVector in adjacentVectors:
-        adjacentPoint = addVectors(point, adjacentVector)
-        if inboundsPoint(heightMap, adjacentPoint):
-            adjacentValue = getValue(heightMap, adjacentPoint)
-            if value >= adjacentValue:
-                return False # Found one where value is greater than adjacent value
-    
+def getLowPointValues(lowPoints):
+    return list(map(lambda x: getValue(heightMap, x), lowPoints))
+
+
+def isLowerThanAdjacents(heightMap, adjacentVectors, point, value):
+    inboundAdjacentPoints = getInboundAdjacentPoints(point, adjacentVectors)
+    for adjacentPoint in inboundAdjacentPoints:
+        adjacentValue = getValue(heightMap, adjacentPoint)
+        if value >= adjacentValue:
+            return False  # Found one where value is greater than adjacent value
+
+
     return True
+
+
+def setProcessedForValue(heightMap, processedMap, value):
+    for r in range(len(heightMap)):
+        for c in range(len(heightMap[r])):
+            point = createVector(c, r)
+            pointValue = getValue(heightMap, point)
+            if pointValue == value:
+                setValue(processedMap, point, True)
+
+def pointIsProcessed(processedMap, point):
+    return getValue(processedMap, point)
+
+def getBasinsPoints(heightMap, processedMap, lowPoints):
+    basinsPoints = []
+    for lowPoint in lowPoints:
+        basinsPoints.append(getBasinPointsFromPoint(
+            heightMap, processedMap, lowPoint))
+
+    return basinsPoints
+
+
+def getBasinPointsFromPoint(heightMap, processedMap, lowPoint):
+    basinPoints = [lowPoint]
+    lowPointValue = getValue(heightMap, lowPoint)
+
+    # print(lowPoint, lowPointValue)
+
+    isNewBasinPoint = False
+
+    inboundAdjacentPoints = getInboundAdjacentPoints(lowPoint, directAdjacentVectors)
+    newBasinPoints = []
+    for adjacentPoint in inboundAdjacentPoints:
+        adjacentPointValue = getValue(heightMap, adjacentPoint)
+        isNewBasinPoint = (not pointIsProcessed(processedMap, adjacentPoint)) and (adjacentPointValue > lowPointValue)
+        if isNewBasinPoint:
+            newBasinPoints.append(adjacentPoint)
+            basinPoints.append(adjacentPoint)
+
+    # Set new basis points processed
+    for newBasinPoint in newBasinPoints:
+        setValue(processedMap, newBasinPoint, True)
+    
+    for newBasinPoint in newBasinPoints:
+        otherBasinPoints = getBasinPointsFromPoint(heightMap, processedMap, newBasinPoint)
+        for otherBasinPoint in otherBasinPoints:
+            if not (otherBasinPoint in basinPoints):
+                basinPoints.append(otherBasinPoint)
+
+    # print(newBasinPoints)
+
+    return basinPoints
 
 
 lines = getInputLines()
 directAdjacentVectors = [
     createVector(-1,  0),
-    createVector( 0, -1),
-    createVector( 1,  0),
-    createVector( 0,  1)
+    createVector(0, -1),
+    createVector(1,  0),
+    createVector(0,  1)
 ]
 
 # Part 1
 heightMap = createHeightMap(lines)
 # print(heightMap)
 
-lowPointValues = getLowPointValues(heightMap)
+lowPoints = getLowPoints(heightMap)
+lowPointValues = getLowPointValues(lowPoints)
 # print(lowPointValues)
 
 lowPointRiskLevels = list(map(lambda x: x + 1, lowPointValues))
@@ -97,5 +176,26 @@ resultPart1 = sum(lowPointRiskLevels)
 print('Anwser day 9 part 1:', resultPart1)
 
 # Part 2
-resultPart2 = 0
+processedMap = createProcessedMap(heightMap)
+setProcessedForValue(heightMap, processedMap, 9)
+basinsPoints = getBasinsPoints(heightMap, processedMap, lowPoints)
+# for basinPoints in basinsPoints:
+#     print(basinPoints)
+
+basinsVolumes = list(map(lambda x: len(x), basinsPoints))
+# print(basinsVolumes)
+
+xLargestVolumes = []
+basinsVolumesCopy = basinsVolumes.copy()
+while (len(xLargestVolumes) < 3) and (len(basinsVolumesCopy) > 0):
+    maxValue = max(basinsVolumesCopy)
+    xLargestVolumes.append(maxValue)
+    basinsVolumesCopy.remove(maxValue)
+
+# print(xLargestVolumes)
+
+resultPart2 = xLargestVolumes[0]
+for i in range(1, len(xLargestVolumes)):
+    resultPart2 *= xLargestVolumes[i]
+
 print('Anwser day 9 part 2:', resultPart2)
