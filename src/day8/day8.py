58c74da2f641549c32ec4b47171a141ee53c1b86
Remco Vorthoren <remco.boys@gmail.com>
2021-12-09 22:07:34 +0100

    Day 8 part 2 - Place the third step in separated functions; End calculation set in separated functions

--- src/day8/day8.py
+++ src/day8/day8.py
@@ -160,27 +160,23 @@ for note in notes:
 
         if numWithUniqueSegmentLength in otherNumsNotToCheckPerNum:
             otherNumsNotToCheck = otherNumsNotToCheckPerNum[numWithUniqueSegmentLength]
-            numsToCheck = list(filter(lambda x: not (
-                x in otherNumsNotToCheck), numsToFind))
+            numsToCheck = list(filter(lambda x: not (x in otherNumsNotToCheck), numsToFind))
             segmentLengthsWithNums = groupSegmentLengthsWithNums(numsToCheck)
             sortedSegmentLengths = sorted(segmentLengthsWithNums.keys())
             for segmentLength in sortedSegmentLengths:
                 signalsForLength = signalsPerLength[segmentLength]
                 SegmentLengthForNums = segmentLengthsWithNums[segmentLength]
-                signalsForLengthToCheck = list(
-                    filter(lambda x: x in signalsToConsider, signalsForLength))
+                signalsForLengthToCheck = list(filter(lambda x: x in signalsToConsider, signalsForLength))
 
-                signalsToUse = list(filter(lambda x: check(
-                    x, newSignalsForNum), signalsForLengthToCheck))
+                signalsToUse = list(filter(lambda x: check(x, newSignalsForNum), signalsForLengthToCheck))
                 if len(signalsToUse) == 1 & len(SegmentLengthForNums) == 1:
-                    print('before ->', signalsPerSegment)
+                    # print('before ->', signalsPerSegment)
                     foundNum = SegmentLengthForNums[0]
                     segmentsForNum = segmentsPerNum[foundNum]
                     consideredSignal = signalsToUse[0]
-                    print(foundNum, segmentsForNum, consideredSignal)
-                    deleteNotPossibleSignalsForSegments(
-                        signalsPerSegment, segmentsForNum, consideredSignal)
-                    print('after ->', signalsPerSegment)
+                    # print(foundNum, segmentsForNum, consideredSignal)
+                    deleteNotPossibleSignalsForSegments(signalsPerSegment, segmentsForNum, consideredSignal)
+                    # print('after ->', signalsPerSegment)
                     numsToFind.remove(foundNum)
                     signalsToConsider.remove(consideredSignal)
 
@@ -219,18 +215,51 @@ def deleteSignalFromSegments(signalsPerSegment, signal):
         signals = ''.join(filter(lambda x: not (x in signal), signals))
         signalsPerSegment[segment] = signals
 
-while True:    
-    segmentToDoWith = getFirstSegmentWithOneSignalLeft(signalsPerUnsolvedSegments)
-    if segmentToDoWith == None:
-        break
+def doStep3(signalsPerUnsolvedSegments, signalPerSegment):
+    while True:    
+        segmentToDoWith = getFirstSegmentWithOneSignalLeft(signalsPerUnsolvedSegments)
+        if segmentToDoWith == None:
+            break
 
-    signalForSegment = signalsPerUnsolvedSegments[segmentToDoWith]
-    signalPerSegment.setdefault(segmentToDoWith, signalForSegment)
-    signalsPerUnsolvedSegments.pop(segmentToDoWith)
-    deleteSignalFromSegments(signalsPerUnsolvedSegments, signalForSegment)
+        signalForSegment = signalsPerUnsolvedSegments[segmentToDoWith]
+        signalPerSegment.setdefault(segmentToDoWith, signalForSegment)
+        signalsPerUnsolvedSegments.pop(segmentToDoWith)
+        deleteSignalFromSegments(signalsPerUnsolvedSegments, signalForSegment)
 
-print(signalsPerUnsolvedSegments)
-print(signalPerSegment)
+doStep3(signalsPerUnsolvedSegments, signalPerSegment)
+# print(signalsPerUnsolvedSegments)
+# print(signalPerSegment)
 
-resultPart2 = 0
+newConfigSignalForSegment = {v: k for k, v in signalPerSegment.items()}
+# print(translateTabelSignalsPerSegment)
+
+numPerSegments = {v: k for k, v in segmentsPerNum.items()}
+# print(numPerSegments)
+
+def sortSignals(signals):
+    return ''.join(sorted(signals))
+
+def translateSignals(signals, table):
+    return ''.join(map(lambda s: table[s], signals))
+
+outputValues = []
+
+def getOutputValue(fourDigitSignals, configSignalForSegment):
+    digits = []
+    for digitSignals in fourDigitSignals:
+        translated = translateSignals(digitSignals, configSignalForSegment)
+        sTranslated = sortSignals(translated)
+        # print(digitSignals, translated, sTranslated)
+        num = numPerSegments[sTranslated]
+        digits.append(num)
+    # print(digits)
+    return int(''.join(map(lambda x: str(x), digits)))
+    
+for note in notes:
+    fourDigitSignals = note[FourDigitOutputValue]
+    outputValue = getOutputValue(fourDigitSignals, newConfigSignalForSegment)
+    outputValues.append(outputValue)
+
+print(outputValues)
+resultPart2 = sum(outputValues)
 print('Anwser day 8 part 2:', resultPart2)
