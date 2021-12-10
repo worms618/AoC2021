9b4efd9d60b009e32516b76ded7669e0ed26368d
Remco Vorthoren <remco.boys@gmail.com>
2021-12-10 08:30:44 +0100

    Day 10 part 1 - solution

--- src/day10/day10.py
+++ src/day10/day10.py
@@ -14,10 +14,91 @@ def getInputLines():
 # --- </Do not edit> ---
 
 
+OpeningCharIndex = 0
+ClosingCharIndex = 1
+
+SyntaxErrorScoreTable = dict()
+SyntaxErrorScoreTable.setdefault(')', 3)
+SyntaxErrorScoreTable.setdefault(']', 57)
+SyntaxErrorScoreTable.setdefault('}', 1197)
+SyntaxErrorScoreTable.setdefault('>', 25137)
+
+ChunkSyntax = dict()
+ChunkSyntax.setdefault('(', ')')
+ChunkSyntax.setdefault('[', ']')
+ChunkSyntax.setdefault('{', '}')
+ChunkSyntax.setdefault('<', '>')
+
 lines = getInputLines()
 
+
+def getChunksFromLine(line, syntax):
+    chunks = []
+
+    queue = []
+    # processedChars = []
+
+    for char in line:
+        isOpeningChar = char in syntax
+        if isOpeningChar:
+            queue.append(char)
+        else:
+            nextChunkOpeningChar = queue.pop()
+            chunks.append((nextChunkOpeningChar, char))
+            # processedChars.append(nextChunkOpeningChar)
+            # processedChars.append(char)
+
+    for leftoverOpeningChar in queue:
+        chunks.append((leftoverOpeningChar, None))
+        # processedChars.append(leftoverOpeningChar)
+
+
+    # print(len(processedChars), len(processedChars))
+    return chunks
+
+
+def getFirstIllegalChar(chunks, syntax):
+    for chunk in chunks:
+        openingChar = chunk[OpeningCharIndex]
+        closingChar = chunk[ClosingCharIndex]
+
+        if (openingChar == None) | (closingChar == None):
+            continue
+
+        expectedClosingChar = syntax[openingChar]
+        if closingChar != expectedClosingChar:
+            return closingChar
+
+    return None
+
+
+def getChunksFromLines(lines, syntax):
+    return list(map(lambda x: getChunksFromLine(x, syntax), lines))
+
+
+def getFirstIllegalCharPerLine(chunksPerLines, syntax):
+    # get the first illegal char from the chunks per line
+    # When no found return None
+    # filter out the None values
+    return list(filter(lambda x: x != None, map(lambda x: getFirstIllegalChar(x, syntax), chunksPerLines)))
+
+
+def getTotalSyntaxErrorScore(chars, scoreTable):
+    return sum(map(lambda x: scoreTable[x], chars))
+
+
 # Part 1
-resultPart1 = 0
+# lines = ['{<({})>]']
+# lines = ['{([(<{}[<>[]}>{[]{[(<()>']
+# lines = ['{([(<{}[<>[]}>{[]{[(<()>', '[({(<(())[]>[[{[]{<()<>>']
+
+chunksPerLines = getChunksFromLines(lines, ChunkSyntax)
+# print(chunksPerLines)
+
+firstIllegalCharsPerLine = getFirstIllegalCharPerLine(chunksPerLines, ChunkSyntax)
+# print(firstIllegalCharsPerLine)
+
+resultPart1 = getTotalSyntaxErrorScore(firstIllegalCharsPerLine, SyntaxErrorScoreTable)
 print('Anwser day 10 part 1:', resultPart1)
 
 # Part 2
