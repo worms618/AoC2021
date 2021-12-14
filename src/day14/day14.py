74b921c3178f23d2dd41f09474b164ca4475c733
Remco Vorthoren <remco.boys@gmail.com>
2021-12-14 18:48:50 +0100

    day 14 part 1 - add solution

--- src/day14/day14.py
+++ src/day14/day14.py
@@ -13,11 +13,83 @@ def getInputLines():
     return lines
 # --- </Do not edit> ---
 
+def getTemplateAndRules(lines):
+    template = lines[0]
+    rules = createRules(lines[2:])
+
+    return (template, rules)
+
+def createRules(lines):
+    rules = dict()
+
+    for line in lines:
+        # print(line)
+        [pair, element] = line.split(' -> ')
+        rules.setdefault(pair, element)
+
+    return rules
+
+def applyInsertionRules(polymer, rules):
+    newPolymer = ''
+    pairs = getPolymerPairs(polymer)
+    # print(pairs)
+
+    for pair in pairs:
+        # print(pair)
+        if pair in rules:
+            if len(newPolymer) == 0:
+                newPolymer = pair[0]
+
+            value = rules[pair]
+            newPart = ''.join([value, pair[1]])
+            # print(newPart)
+            newPolymer += newPart
+
+    return newPolymer
+
+def getPolymerPairs(polymer):
+    pairs = []
+
+    for i in range(len(polymer) - 1):
+        pairs.append(polymer[i:i+2])
+
+    return pairs
+
+def countElements(polymer):
+    countPerElement = dict()
+
+    for c in polymer:
+        if not (c in countPerElement):
+            countPerElement.setdefault(c, 0)
+        
+        countPerElement[c] += 1
+
+    return countPerElement
 
 lines = getInputLines()
+[template, rules] = getTemplateAndRules(lines)
 
+# print(template)
+# print(rules)
+
+steps = 10
+
+polymer = template
+# print('start ->', polymer)
+for _ in range(steps):
+    polymer = applyInsertionRules(polymer, rules)
+    # print('after ->', polymer)
 # Part 1
-resultPart1 = 0
+
+countPerElement = countElements(polymer)
+# print(countPerElement)
+
+counts = countPerElement.values()
+mostCommonElementCount = max(counts)
+leastCommonElementCount = min(counts)
+print(mostCommonElementCount, '-', leastCommonElementCount)
+
+resultPart1 = mostCommonElementCount - leastCommonElementCount
 print('Anwser day 14 part 1:', resultPart1)
 
 # Part 2
