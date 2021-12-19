f6ff90580d9b269e23472f0445de72a71af9503d
Remco Vorthoren <remco.boys@gmail.com>
2021-12-19 16:38:54 +0100

    day 19 - add files

---
+++ src/day19/day19.py
@@ -0,0 +1,25 @@
+# --- <Do not edit> ---
+def getInputLines():
+    import sys
+
+    if len(sys.argv) < 1:
+        raise ValueError('Path of this script not available in sys.argv')
+
+    lines = []
+    inputFilePath = sys.argv[0].replace('day19.py', 'input.txt')
+    with open(inputFilePath) as f:
+        lines = [line.strip() for line in f]
+
+    return lines
+# --- </Do not edit> ---
+
+
+lines = getInputLines()
+
+# Part 1
+resultPart1 = 0
+print('Anwser day 19 part 1:', resultPart1)
+
+# Part 2
+resultPart2 = 0
+print('Anwser day 19 part 2:', resultPart2)
